a758936f4fc25df963c9f431fdda45a280907fdd
Alexandre Angrignon <alex040892@gmail.com>
2025-12-09 14:42:13 +1300

    tailwind setup and nuxt font setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 import { repositoryName, apiEndpoint } from './slicemachine.config.json';
-
+import tailwindcss from '@tailwindcss/vite';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
@@ -8,9 +8,40 @@ export default defineNuxtConfig({
     head: {
       title: 'Prismic + Nuxt Minimal Starter',
       htmlAttrs: { lang: 'en' },
-      meta: [{ charset: 'utf-8' }],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    }
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'apple-mobile-web-app-title', content: 'Farbe' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        {
+          rel: 'apple-touch-icon',
+          href: '/apple-touch-icon.png',
+          sizes: '180x180',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon-96x96.png',
+          sizes: '96x96',
+        },
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          href: '/favicon.png',
+        },
+        {
+          rel: 'manifest',
+          href: '/site.webmanifest',
+        },
+      ],
+    },
+  },
+
+  css: ['@/assets/styles/index.css'],
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 
   modules: ['@nuxt/eslint', '@nuxtjs/prismic'],
@@ -29,9 +60,9 @@ export default defineNuxtConfig({
           uid: 'home',
           path: '/',
         },
-      ]
-    }
+      ],
+    },
   },
 
   compatibilityDate: '2025-07-16',
-})
+});
