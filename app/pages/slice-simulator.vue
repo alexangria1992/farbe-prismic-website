33570300220c5ac3903ab8b94d89d1230a23dcec
Alexandre Angrignon <alex040892@gmail.com>
2026-01-07 10:30:52 +1300

    add blank update

--- app/pages/slice-simulator.vue
+++ app/pages/slice-simulator.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-import { SliceSimulator } from "@slicemachine/adapter-nuxt/simulator";
-import { components } from "~/slices";
+import { SliceSimulator } from '@slicemachine/adapter-nuxt/simulator';
+import { components } from '~/slices';
+
+definePageMeta({
+  layout: 'blank',
+});
 </script>
 
 <template>
-  <SliceSimulator v-slot="{ slices }">
-    <SliceZone
-      :slices="slices"
-      :components="components"
-    />
+  <SliceSimulator v-slot="{ slices }" background="transparent">
+    <SliceZone :slices="slices" :components="components" />
   </SliceSimulator>
 </template>
