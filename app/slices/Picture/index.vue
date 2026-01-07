dc3c1174a49c48b58ef7fef3be8ee6360fbe3021
Alexandre Angrignon <alex040892@gmail.com>
2026-01-08 11:54:43 +1300

    Add rich text and picture slice

--- app/slices/Picture/index.vue
+++ app/slices/Picture/index.vue
@@ -1,30 +1,56 @@
 <script setup lang="ts">
-import type { Content } from "@prismicio/client";
+import type { Content } from '@prismicio/client';
 
 // The array passed to `getSliceComponentProps` is purely optional.
 // Consider it as a visual hint for you when templating your slice.
-defineProps(
-  getSliceComponentProps<Content.PictureSlice>([
-    "slice",
-    "index",
-    "slices",
-    "context",
-  ]),
-);
+defineProps(getSliceComponentProps<Content.PictureSlice>());
 </script>
 
 <template>
   <section
+    class="grid xl:grid-cols-[3fr_2fr]"
     :data-slice-type="slice.slice_type"
     :data-slice-variation="slice.variation"
   >
-    Placeholder component for picture (variation: {{ slice.variation }}) slices.
-
-    <br />
-    <strong>You can edit this slice directly in your code editor.</strong>
-    <!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
--->
+    <figure class="contents">
+      <PrismicImage
+        :field="slice.primary.picture"
+        class="z-20 w-full row-span-2"
+        loading="lazy"
+      />
+      <figcaption
+        class="px-4 pt-4 pb-16 rich-text"
+        :class="{
+          'xl:self-start':
+            slice.variation === 'default' || slice.variation === 'bottom',
+          'xl:self-end xl:order-last': slice.variation === 'top',
+        }"
+      >
+        <PrismicRichText :field="slice.primary.caption" />
+      </figcaption>
+    </figure>
+    <figure
+      v-if="$prismic.isFilled.image(slice.primary.secondary_picture)"
+      class="flex flex-col"
+      :class="{
+        'xl:self-end':
+          slice.variation === 'default' || slice.variation === 'bottom',
+        'xl:self-start ': slice.variation === 'top',
+      }"
+    >
+      <PrismicImage
+        :field="slice.primary.secondary_picture"
+        class="z-20 w-full"
+        loading="lazy"
+      />
+      <figcaption
+        class="px-4 pt-4 pb-16 rich-text"
+        :class="{
+          'xl:order-first': slice.variation === 'bottom',
+        }"
+      >
+        <PrismicRichText :field="slice.primary.secondary_caption" />
+      </figcaption>
+    </figure>
   </section>
 </template>
