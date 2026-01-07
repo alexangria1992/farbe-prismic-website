e917b717f4551aa403fc923904e22f589fd756a8
Alexandre Angrignon <alex040892@gmail.com>
2026-01-07 14:53:14 +1300

    Templating Rich Text Slice

--- app/slices/RichText/index.vue
+++ app/slices/RichText/index.vue
@@ -5,10 +5,16 @@ defineProps(getSliceComponentProps<Content.RichTextSlice>());
 </script>
 
 <template>
-  <section class="bounded rich-text">
+  <section
+    class="bounded rich-text flex flex-col justify-center"
+    :class="{
+      'min-h-[40vh]': slice.variation !== 'fullscreen',
+      'min-h-screen': slice.variation === 'fullscreen',
+    }"
+  >
     <PrismicRichText :field="slice.primary.title" />
     <PrismicRichText :field="slice.primary.text" />
-    <div v-if="slice.primary?.ctas?.length" class="mt-16 flex -ml-4">
+    <div v-if="slice.primary?.ctas?.length" class="mt-16 flex f -ml-4">
       <PrismicLink
         v-for="link in slice.primary.ctas"
         :key="link.key"
