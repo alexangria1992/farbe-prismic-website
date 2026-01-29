7418fb7b80257f60550252725322cfda03e568af
Alex <alex040892@gmail.com>
2026-01-30 11:28:31 +1300

    test

--- app/slices/Cart/index.vue
+++ app/slices/Cart/index.vue
@@ -1,30 +1,16 @@
 <script setup lang="ts">
-import type { Content } from "@prismicio/client";
+import type { Content } from '@prismicio/client';
 
-// The array passed to `getSliceComponentProps` is purely optional.
-// Consider it as a visual hint for you when templating your slice.
-defineProps(
-  getSliceComponentProps<Content.CartSlice>([
-    "slice",
-    "index",
-    "slices",
-    "context",
-  ]),
-);
+defineProps(getSliceComponentProps<Content.CartSlice>());
 </script>
 
 <template>
-  <section
-    :data-slice-type="slice.slice_type"
-    :data-slice-variation="slice.variation"
-  >
-    Placeholder component for cart (variation: {{ slice.variation }}) slices.
-
-    <br />
-    <strong>You can edit this slice directly in your code editor.</strong>
-    <!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
--->
-  </section>
+  <SlideIn
+    id="cart"
+    as="from"
+    class="bounded rich-text min-h-screen flex flex-col"
+    method="POST"                                                   
+    action="/api/"          
+  >             
+  </SlideIn>
 </template>
